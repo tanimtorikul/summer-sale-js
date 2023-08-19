6682915de3179ce13f843ebfc72a60bd47f69acc
torikulswe <torikulswe@gmail.com>
2023-08-20 05:23:54 +0600

    Initially completed

---
+++ js/script.js
@@ -0,0 +1,70 @@
+let total = 0;
+
+function handleClick(target) {
+    // set the product title as an ordered list
+    const productTitle = target.querySelector('.card-title').innerText;
+    const li = document.createElement('li');
+    li.innerText = productTitle;
+    const selectedItemContainer = document.getElementById('list-container');
+    const liNumber = selectedItemContainer.children.length + 1;
+    li.innerText = liNumber + '. ' + productTitle;
+    selectedItemContainer.appendChild(li);
+
+    // set the price to total price
+    const price = target.querySelector('.price').innerText.split(' ')[0];
+    total = parseFloat(total) + parseFloat(price);
+    const firstTotalPrice = document.getElementById('first-total-price');
+    firstTotalPrice.innerText = total.toFixed(2);
+
+    // set the grand total price
+    const grandTotal = document.getElementById('grand-total-price');
+    grandTotal.innerText = parseFloat(firstTotalPrice.innerText).toFixed(2);
+
+
+    // purchase button enable
+    const purchaseBtn = document.getElementById('purchase-btn');
+    if (firstTotalPrice.innerText > 0) {
+        purchaseBtn.disabled = false;
+    }
+    else {
+        purchaseBtn.disabled = true;
+    }
+
+    // apply button enable
+    const applyCouponButton = document.getElementById("apply-coupon-btn");
+
+    if (firstTotalPrice.innerText > 200) {
+        applyCouponButton.disabled = false;
+    }
+    else {
+        applyCouponButton.disabled = true;
+    }
+
+
+
+    // applying coupon code to calculate discount
+    document.getElementById('apply-coupon-btn').addEventListener('click', function () {
+        const couponField = document.getElementById('coupon-field');
+        const discountPrice = document.getElementById('discount-price');
+        if (couponField.value === 'SELL200') {
+            const discountMoney = parseFloat(firstTotalPrice.innerText) * 0.2;
+            discountPrice.innerText = discountMoney.toFixed(2);
+        }
+
+
+        couponField.value = '';
+
+        const grandTotal = document.getElementById('grand-total-price');
+        grandTotal.innerText = parseFloat(firstTotalPrice.innerText) - parseFloat(discountPrice.innerText).toFixed(2);
+
+    })
+    // go home but when clicked go to home page and refresh data
+    document.getElementById('go-home-btn').addEventListener('click', function () {
+        window.location.href = 'index.html';
+    })
+
+
+
+}
+
+
